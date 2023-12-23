726f0ad3c7314abe31803cee91f2078224e6ca7e
th1nkers <suryasingh11112003@gmail.com>
2023-12-23 11:18:36 +0530

    back image

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ const app = express();
 
 app.use(bodyParser.json());
 
-// app.use('/uploads/images', express.static(path.join('uploads', 'images')));
+app.use('/uploads/images', express.static(path.join('uploads', 'images')));
 
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
